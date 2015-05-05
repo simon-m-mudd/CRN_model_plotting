3cf04f3d631c6667f8a19882a65cfea0fdae6e89
Simon Marius Mudd <simon.m.mudd@ed.ac.uk>
2015-05-05 11:31:12 +0200

    Updated the plotting function

--- CRN_basin_average_plotting.py
+++ CRN_basin_average_plotting.py
@@ -26,7 +26,8 @@ def CRN_model_plotting():
     #
     ########################
     DataDirectory = "C://basin_data//Model_results//"
-    FileName = "CRNvariable_padova_1_0_0.er_frame_metadata"
+    FilePrefix = "CRNvariable_short_0_0_1var"
+    FileName = FilePrefix+".er_frame_metadata"
     
     
     FileName = DataDirectory+FileName
@@ -46,7 +47,6 @@ def CRN_model_plotting():
     #print DataLines
     
     
-    
     # initiate the variables
     D = np.zeros(n_lines-2)
     K = np.zeros(n_lines-2)
@@ -59,7 +59,7 @@ def CRN_model_plotting():
     i = 0
     for line in DataLines:
         splitline = line.split("\t")
-        print splitline
+        #print splitline
         D[i] = float(splitline[3])
         K[i] = float(splitline[2])
         T[i] = float(splitline[1])/1000     # in kyr
@@ -68,9 +68,12 @@ def CRN_model_plotting():
         Eapp[i] = float(splitline[6])*1000     # in mm/yr
         i= i+1
     
-    print T
-    print U
-    print E
+    #print T
+    #print U
+    #print E
+    
+    print "mean e is: " + str(E.mean())
+    print "mean Eapp is: " + str(Eapp.mean())
     
     # now plot the results    
     fig = pp.figure(1, facecolor='white',figsize=(10,7.5))
@@ -79,12 +82,15 @@ def CRN_model_plotting():
     pp.plot(T,E,linewidth=3,label = ("Erosion rate, $mm/yr$"))
     pp.plot(T,U,linewidth=3,label = ("Uplift rate, $mm/yr$"))
     pp.plot(T,Eapp,linewidth=3,label = ("Apparent erosion rate, $mm/yr$"))
-    pp.legend(loc=2)
+    #pp.legend(loc=2)
+    leg = pp.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=3, mode="expand", borderaxespad=0.)
 
     pp.rcParams['xtick.direction'] = 'in'
     pp.rcParams['ytick.direction'] = 'in'
     #ax1.set_xscale('log')
     #ax1.set_yscale('log')
+       
   
   
     ax1.spines['top'].set_linewidth(2.5)
@@ -100,12 +106,53 @@ def CRN_model_plotting():
     for line in ax1.get_yticklines():
         line.set_marker(mpllines.TICKRIGHT)
 
-    pp.xlabel('Time ($kyr$)',fontsize = axis_size)
-    pp.ylabel('Uplift and erosion rates $mm/yr$)',fontsize = axis_size) 
+    ax1.set_xlabel('Time ($kyr$)',fontsize = axis_size)
+    ax1.set_ylabel('Uplift and erosion rates $mm/yr$)',fontsize = axis_size) 
+    #pp.title("$A$ is "+str(A)+ " $m^2/g$")
+    #pp.ylim(0,1)
+    ax1.set_xlim(200,300)
+
+    figname = FilePrefix+"_erosion.svg"
+    pp.savefig(figname, format='svg')  
+
+
+    fig2 = pp.figure(1, facecolor='white',figsize=(10,7.5))
+    ax2 = fig.add_subplot(2,1,1)  
+    
+    ax2.plot(T,D,linewidth=3,label = ("D, $m^2/yr$"))
+    
+    ax3 = ax2.twinx()    
+    
+    ax3.plot(T,K,linewidth=3,label = ("K, $variable units$"))
+    #ax2.plot(T,U,linewidth=3,label = ("Apparent erosion rate, $mm/yr$"))
+    pp.legend(loc=2)
+
+    pp.rcParams['xtick.direction'] = 'in'
+    pp.rcParams['ytick.direction'] = 'in'
+    #ax1.set_xscale('log')
+    #ax1.set_yscale('log')
+  
+  
+    ax2.spines['top'].set_linewidth(2.5)
+    ax2.spines['left'].set_linewidth(2.5)
+    ax2.spines['right'].set_linewidth(2.5)
+    ax2.spines['bottom'].set_linewidth(2.5) 
+    ax2.tick_params(axis='both', width=2.5)    
+    #ax1.xaxis.set_major_formatter(FormatStrFormatter('%.03f'))
+
+    for line in ax2.get_xticklines():
+        line.set_marker(mpllines.TICKUP)
+
+    for line in ax2.get_yticklines():
+        line.set_marker(mpllines.TICKRIGHT)
+
+    ax2.set_xlabel('Time ($kyr$)',fontsize = axis_size)
+    ax2.set_ylabel('D $m^2/yr$',fontsize = axis_size)
+    ax3.set_ylabel('K')
     #pp.title("$A$ is "+str(A)+ " $m^2/g$")
     #pp.ylim(0,1)
 
-    figname = "Cosmo_Plot.svg"
+    figname = FilePrefix+"_forcing.svg"
     pp.savefig(figname, format='svg')       
         
 if __name__ == "__main__":
