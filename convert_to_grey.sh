597c2e2f93aa54d2c4ea823da6430dfb3cc84870
simon-m-mudd <simon.m.mudd@ed.ac.uk>
2016-02-09 16:55:49 +0000

    Added a little script for changing figures to greyscale

---
+++ convert_to_grey.sh
@@ -0,0 +1,10 @@
+gs \
+ -sOutputFile=output.pdf \
+ -sDEVICE=pdfwrite \
+ -sColorConversionStrategy=Gray \
+ -dProcessColorModel=/DeviceGray \
+ -dCompatibilityLevel=1.4 \
+ -dNOPAUSE \
+ -dBATCH \
+ $1
+
